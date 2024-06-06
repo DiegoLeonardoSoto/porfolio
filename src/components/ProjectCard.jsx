da0db425e3bac75d83ca5cad259b3e5323f4ce3c
Diego Soto <88800678+DiegoLeonardoSoto@users.noreply.github.com>
2024-06-06 15:34:11 -0300

    update projects.json

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,7 +2,14 @@ import { LiveIcon } from "../assets";
 import { CodeIcon } from "../assets/CodeIcon";
 import { Link } from "./Link";
 
-export const ProjectCard = ({ title, description, codeUrl, img, tags }) => {
+export const ProjectCard = ({
+  title,
+  description,
+  codeUrl,
+  liveUrl,
+  img,
+  tags,
+}) => {
   return (
     <div
       className={`flex flex-col gap-4 rounded-2xl border-2 border-black bg-white p-8 shadow-[6px_6px_25px_-7px_rgba(0,0,0,0.75)]`}
@@ -10,28 +17,43 @@ export const ProjectCard = ({ title, description, codeUrl, img, tags }) => {
       <h4 className="border-b-2 border-black text-2xl font-bold tracking-widest">
         {title.toUpperCase()}
       </h4>
-      <p className="text-sm text-neutral-600">{description}</p>
-
-      <div className="h-72 w-full rounded-2xl bg-gradient-to-r from-purple-500 to-pink-500">
-        {img}
+      <p className="text-sm text-neutral-600">{description["spanish"]}</p>
+      <div className="flex items-center gap-4 overflow-hidden text-sm text-neutral-700">
+        {tags.map((tag) => {
+          return (
+            <span key={tag} className="rounded-full bg-gray-200 px-3 py-2">
+              {tag}
+            </span>
+          );
+        })}
       </div>
-      <div className="flex justify-between">
-        <div className="flex w-1/3 items-center gap-4 overflow-hidden text-sm text-neutral-700">
-          <span> React</span>
-          <span>TailwindCSS</span>
-          <span>Firebase</span>
-        </div>
 
+      {/* 
+      <img src={img} alt={title + "Image"} className="h-72 rounded-2xl" />
+      */}
+
+      <div
+        style={{
+          backgroundImage: `url('${img}')`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className={`h-72 w-full rounded-2xl bg-[url()]`}
+      ></div>
+      <div className="flex justify-end">
         <div className="flex gap-4">
           <Link
             name="Code"
             hoverStyle="after:bg-blue-600 hover:bg-blue-600"
             icon={<CodeIcon />}
+            href={codeUrl}
           />
           <Link
             name="Live"
             hoverStyle="after:bg-violet-600 hover:bg-violet-600"
             icon={<LiveIcon />}
+            href={liveUrl}
           />
         </div>
       </div>
